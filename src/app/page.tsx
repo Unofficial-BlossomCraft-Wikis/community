2eb0942c9fc525283f280a38ea9d235e4f5f80f4
altie122 <77561831+altie122@users.noreply.github.com>
2024-09-09 23:42:54 -0600

    add more info to the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import { getOAuthToken } from "~/lib/auth/discord";
 import { getUserNickname } from "~/lib/getusernickname";
 import Image from "next/image";
 import { data } from "~/server/queries";
-import BCUW from '../../public/bcuwTitleNormal.png'
+import BCUW from "../../public/bcuwTitleNormal.png";
+import Link from "next/link";
+import { ExternalLink } from "lucide-react";
 
 export default async function HomePage() {
   const user = await currentUser();
@@ -40,6 +42,8 @@ export default async function HomePage() {
                 BlossomCraft Unofficial Wiki
                 <br />
                 V4 Alpha
+                <br/>
+                Owned and maintained by Altie122
               </p>
             </div>
             <div className="flex items-center justify-center md:basis-1/2">
@@ -58,10 +62,53 @@ export default async function HomePage() {
           </div>
           <div className="text-center">
             {userNickname ? (
-              <p>Your Blossomcraft discord server nickname is {userNickname}</p>
+              <p>Welcome {userNickname}!</p>
             ) : (
-              <p>You are not in the Blossomcraft discord server</p>
+              <p className="flex flex-row items-center justify-center">
+                Please sign up and/or join the&#32;
+                <Link
+                  href="https://discord.gg/blossomcraft"
+                  className="prose-a row flex"
+                >
+                  official BlossomCraft Discord server{" "}
+                  <ExternalLink width={16} />
+                </Link>
+                &#32;to use this site.
+              </p>
             )}
+          </div>
+          <div className="prose">
+            <h2>Whats coming in V4?</h2>
+            <p>Here are some currently planned features:</p>
+            <ul>
+              <li>Completely redesigned and rewritten site</li>
+              <li>Trading marketplace</li>
+              <li>Advanced Player Warp Pages</li>
+              <li>
+                Complete wiki of the server including but not limited to: Items,
+                Crates and Commands.
+              </li>
+              <li>
+                Accounts for features such as Trading and the player warp
+                registry (<b className="text-red-600">REMEMBER:</b> this is an
+                unofficial site, this account is in no way connected to
+                BlossomCraft and is not stored on blossomcraft run servers.)
+              </li>
+            </ul>
+            <h2>What is the BCUW?</h2>
+            <p>
+              This wiki Was originally created to help make seeing all of the
+              amazing Blossom Items easier, this includes:
+            </p>
+            <ul>
+              <li>crates</li>
+              <li>crate keys</li>
+              <li>crate Blossom Items</li>
+              <li>non-crate Blossom Items</li>
+            </ul>
+            <p>
+              But the wiki has started being more of a general-purpose Wiki. and now it is gaining more features like a Player Warp Registry and Trading Marketplace!
+            </p>
           </div>
         </main>
       </div>
