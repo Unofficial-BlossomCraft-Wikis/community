e8f0930a66194ec31f30e6cf6496a986ba047ebe
altie122 <77561831+altie122@users.noreply.github.com>
2024-09-14 23:36:30 -0600

    make user update revalidate path

--- src/app/user/settings/actions.ts
+++ src/app/user/settings/actions.ts
@@ -1,10 +1,12 @@
 'use server'
 
+import { revalidatePath } from "next/cache";
 import { data } from "~/server/queries";
 
  
 export async function updateUserData(id: string, token: string) {
   await data.post.user.updateAllDiscordData(id, token);
+  revalidatePath("/user/settings");
 }
 
 export async function deleteUser(id: string) {
