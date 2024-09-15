1645f5e9011eb8fd882753c3a2b02a539fa55f3c
altie122 <77561831+altie122@users.noreply.github.com>
2024-09-15 01:30:19 -0600

    fix small error on items/[slug]

--- src/app/wiki/items/[slug]/page.tsx
+++ src/app/wiki/items/[slug]/page.tsx
@@ -1,10 +1,10 @@
-import Link from "next/link";
-import { data } from "~/server/queries";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Card, CardContent } from "~/components/ui/card";
-import { Separator } from "@/components/ui/separator";
-import { Button } from "~/components/ui/button";
-import { items } from "~/lib/items";
+// import Link from "next/link";
+// import { data } from "~/server/queries";
+// import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+// import { Card, CardContent } from "~/components/ui/card";
+// import { Separator } from "@/components/ui/separator";
+// import { Button } from "~/components/ui/button";
+// import { items } from "~/lib/items";
 import { redirect } from "next/navigation";
 
 export default async function HomePage({
@@ -13,14 +13,14 @@ export default async function HomePage({
   params: { slug: string };
 }) {
   const { slug } = params;
-  const item = items.find((item) => item.slug == slug);
-  if (item == undefined) {
+  // const item = items.find((item) => item.slug == slug);
+  // if (item == undefined) {
     redirect(`/wiki/items?inf=true&infslug=${slug}`);
-  }
+  // }
   return (
     <main className="bg-background mx-2 mb-2 h-full w-full scroll-auto rounded-lg p-2">
-      <h1 className="text-3xl">{item.name}</h1>
-      <Tabs defaultValue="img" className="w-[400px]">
+      {/* <h1 className="text-3xl">{item.name}</h1> */}
+      {/* <Tabs defaultValue="img" className="w-[400px]">
         <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="img">Image</TabsTrigger>
           {item.image_url == item.mono_image_url ? (
@@ -50,7 +50,7 @@ export default async function HomePage({
       </Tabs>
       <Button variant="outline" asChild>
         <Link href={`/wiki/crates/${item.crate_slug}`}>To {item.crate}</Link>
-      </Button>
+      </Button> */}
     </main>
   );
 }
